5be08a0897fb5bb8d36700588fc1da7aa4abb1e0
Brian Faust <hello@brianfaust.me>
2018-07-02 13:35:47 +0300

    feat: message signing & verifying

--- ark_crypto/message.go
+++ ark_crypto/message.go
@@ -5,10 +5,55 @@
 
 package crypto
 
-func SignMessage(message string, secret string) {
+import (
+    "crypto/sha256"
+    "encoding/hex"
+)
 
+type Message struct {
+    PublicKey string `json:"publickey"`
+    Signature string `json:"signature"`
+    Message   string `json:"message"`
 }
 
-func VerifyMessage(message string, public_key string, signature string) {
+func SignMessage(message string, secret string) (*Message, error) {
+    privateKey, err := PrivateKeyFromSecret(secret, NETWORKS_DEVNET)
 
+    if err != nil {
+        return nil, err
+    }
+
+    hash := sha256.New()
+    _, err = hash.Write([]byte(message))
+
+    if err != nil {
+        return nil, err
+    }
+
+    signature, err := privateKey.Sign(hash.Sum(nil))
+
+    if err != nil {
+        return nil, err
+    }
+
+    return &Message{
+        PublicKey: hex.EncodeToString(privateKey.PublicKey.Serialise()),
+        Signature: hex.EncodeToString(signature),
+        Message: message,
+    }, nil
+}
+
+func VerifyMessage(message *Message) (bool, error) {
+    publicKey, _ := PublicKeyFromBytes(hexDecode(message.PublicKey), NETWORKS_DEVNET)
+
+    hash := sha256.New()
+    _, err := hash.Write([]byte(message.Message))
+
+    if err != nil {
+        return false, err
+    }
+
+    verified, _ := publicKey.Verify(hexDecode(message.Signature), hash.Sum(nil))
+
+    return verified, nil
 }
