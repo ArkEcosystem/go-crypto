d11d2d5f0229e43e2094e96d10ad489eecee1a0c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-11-03 22:44:20 +0000

    fix: vendor field hex length

--- crypto/serializer.go
+++ crypto/serializer.go
@@ -60,9 +60,7 @@ func serializeVendorField(buffer *bytes.Buffer, transaction *Transaction) *bytes
 		_ = binary.Write(buffer, binary.LittleEndian, uint8(vendorFieldLength))
 		_ = binary.Write(buffer, binary.LittleEndian, []byte(transaction.VendorField))
 	} else if len(transaction.VendorFieldHex) > 0 {
-		vendorFieldHexLength := len(transaction.VendorFieldHex)
-
-		_ = binary.Write(buffer, binary.LittleEndian, uint8(vendorFieldHexLength))
+		_ = binary.Write(buffer, binary.LittleEndian, uint8(len(transaction.VendorFieldHex)/2))
 		_ = binary.Write(buffer, binary.LittleEndian, HexDecode(transaction.VendorFieldHex))
 	} else {
 		_ = binary.Write(buffer, binary.LittleEndian, HexDecode("00")[0])
