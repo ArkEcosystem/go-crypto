098779bc0591d70dd8caf2ee575b136755daf021
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-09-23 13:19:35 -0600

    add private key generation

--- crypto/structs.go
+++ crypto/structs.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/btcsuite/btcd/btcec"
+	blst "github.com/supranational/blst/bindings/go"
 )
 
 type FlexToshi uint64
@@ -164,4 +165,8 @@ type HtlcClaimAsset struct {
 
 type HtlcRefundAsset struct {
 	LockTransactionId string `json:"lockTransactionId,omitempty"`
+}
+
+type BLSPrivateKey struct {
+	PrivateKey *blst.SecretKey
 }
--- go.mod
+++ go.mod
@@ -15,5 +15,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/supranational/blst v0.3.13 // indirect
+	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
