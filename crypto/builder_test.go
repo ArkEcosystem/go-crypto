412d68ccd13efe43322403273ff6d344038f2686
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-09-25 10:14:19 -0600

    Update builder_test.go

--- crypto/builder_test.go
+++ crypto/builder_test.go
@@ -266,7 +266,6 @@ func TestBuildValidatorRegistrationWithInvalidKeyLength(t *testing.T) {
 			&Transaction{
 				Asset: &TransactionAsset{
 					Validator: &ValidatorAsset{
-						// ValidatorPublicKey: "b08058db53e2665c84a40f5152e76dd2b652125a6079130d4c315e728bcf4dd1dfb44ac26e82302331d61977d3141118",
 						ValidatorPublicKey: "b08058db53e2665c84a40f5152e76dd2b65212",
 					},
 				},
