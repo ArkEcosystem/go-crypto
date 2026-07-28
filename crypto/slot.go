4306c5e16a0633ddaec30597440b0bb4ba4b59af
Shahin Safaraliyev <shahin@ardenthq.com>
2026-07-28 16:08:32 +0400

    chore: remove header comment from files

--- crypto/slot.go
+++ crypto/slot.go
@@ -1,10 +1,3 @@
-// This file is part of Ark Go Crypto.
-//
-// (c) Ark Ecosystem <info@ark.io>
-//
-// For the full copyright and license information, please view the LICENSE
-// file that was distributed with this source code.
-
 package crypto
 
 import "time"
